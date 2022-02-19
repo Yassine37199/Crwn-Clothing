11500fd2e04f29e28bff20127629e8a310cf1158
Yassine37199 <yassine.ouri7319@gmail.com>
2022-02-19 20:13:17 +0100

    integrated react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Homepage from './Pages/homepage/homepage.component';
+
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Routes>
+        <Route path='/*' element={ <Homepage />} />
+      </Routes>
+      
     </div>
   );
 }
