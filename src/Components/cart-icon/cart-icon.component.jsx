528759f15e83dee3d9b55952657ff90660eaf016
Yassine37199 <yassine.ouri7319@gmail.com>
2022-03-24 15:08:28 +0100

    added cart selectors w/ reselect

--- src/Components/cart-icon/cart-icon.component.jsx
+++ src/Components/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import {ReactComponent as ShoppingIcon} from '../../Assets/Logos/shopping-bag.svg'
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors'
 import './cart-icon.styles.scss'
 
 
-const CartIcon = () => (
+const CartIcon = ({itemCount}) => (
     <div className='cart-icon'>
         <ShoppingIcon className='cart-icon__shopping-Icon' />
-        <span className='cart-icon__item-count'> 0 </span>
+        <span className='cart-icon__item-count'> {itemCount} </span>
     </div>
 )
 
-export default CartIcon;
+const mapStateToProps = (state) => ({
+    itemCount : selectCartItemsCount(state)
+})
+ 
+export default connect(mapStateToProps)(CartIcon);
--- src/Components/cart-item/cart-item.component.jsx
+++ src/Components/cart-item/cart-item.component.jsx
@@ -7,7 +7,7 @@ import './cart-item.styles.scss'
 
 const CartItem = ({name , price , quantity , imageUrl}) => (
     <div className='cart-item'>
-        <img className='cart-item__image' src={imageUrl} alt={name}/>
+        <img className='cart-item__image ' src={imageUrl} alt={name}/>
         <div className='cart-item__details'>
             <p className='cart-item__name'>{name}</p>
             <span className='cart-item__price'>${price}</span>
